23b5177961c94fc12cd9fcf49b4f43b2ef21e2df
Tobiasz Fuczek <tobfuc1412@gmail.com>
2024-02-06 14:22:56 +0100

    Add new class and new def

--- Objectivity/exercise_1.py
+++ Objectivity/exercise_1.py
@@ -20,3 +20,17 @@ person_1.one_year_add()
 print(f"Wiek zwiekszony o 1 rok: {person_1.wiek}")
 
 
+class Student(Person):
+    def __init__(self,imie, nazwisko, wiek, index_id):
+        super().__init__(imie,nazwisko,wiek)
+        self.index_id= index_id
+
+    def show_all_information(self):
+        print(f"Informacje o Studencie: {self.imie}, {self.nazwisko}, {self.wiek}, {self.index_id}")
+
+    def index_Student(self):
+        print(f"Index Studenta: {self.index_id}")
+
+student_1= Student("Piotr", "Zielinski", 33, 5543)
+student_1.show_all_information()
+student_1.index_Student()
